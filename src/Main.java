c66fe612048545910f2400b59bd76becafcc5c6b
Ranjith Kumar <121955754+Ranjithkumar444@users.noreply.github.com>
2026-01-02 13:00:01 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,44 @@
+//FIRST QUESTION
+import java.util.*;
 
+class Main {
+    public static void main(String[] args) {
+        int[] arr = {3,2,2,3};
+        
+        int val = 3;
+        
+        int count = returnK(arr,val);
+        System.out.println(Arrays.toString(arr));
+        System.out.println(count);
+    }
+    
+    public static int returnK(int[] nums,int val){
+        int n = nums.length;
+        int[] ans = new int[n];
+        Arrays.fill(ans,-1);
+        int i = 0;
+        while(i != n){
+            if(nums[i] != val){
+                ans[i] = nums[i];
+            }
+            i++;
+        }
+        int count = 0;
+        for(int j=0;j<n;j++){
+            nums[j] = ans[j];
+            if(nums[j] != -1){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+    
+}
+
+
+
+//SECOND QUESTION
 import java.util.*;
 
 class Main {
